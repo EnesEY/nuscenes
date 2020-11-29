a5d27ffd50dbf7761a58f4ebcc71f811903fab06
Enes.Yaldiz <enes.yaldiz.bevelop@hd.digital>
2020-11-29 17:08:33 +0100

    added-gitignore

--- src/main/loadMovedBefore.py
+++ src/main/loadMovedBefore.py
@@ -7,49 +7,40 @@ from nuscenes.map_expansion import arcline_path_utils
 import matplotlib.pyplot as plt
 import tqdm
 import numpy as np
+import loadInitialMovingState 
 import parameters
 
-#setup
-nusc = NuScenes(version='v1.0-mini', dataroot='/data/sets/nuscenes', verbose=True)
-client=MongoClient('mongodb+srv://enesey:485f6483e3c8666b72fda603a7f87006b83549a54395f2504eb58935f35d00d9@nuscenescluster.jh1vw.mongodb.net/test')
-nusc_map = NuScenesMap(dataroot='/data/sets/nuscenes', map_name='singapore-onenorth')
-db = client.nuscenes
-sample_annotation = db["sample_annotation"]
- 
- # gets the information weather this instance has moved before or not
-def getMovedBefore(sample_annotation):
-    # get first sample annotation of an instance and look if it is moving
-    # input the state into the sample annotation
-    movedBefore = False
-    for instance in nusc.instance.find():
-
+# loads the attribute movedBefore into every sample_annotation
+def loadMovedBefore():
+    print('start adding moved before annotation')
+    # load moving state first
+    loadInitialMovingState.induceMovingStateInSampleAnnotation()
+    for instance in loadInitialMovingState.nusc.instance:
+        # information weather this instance has moved or not
+        movedBefore = False
+        # first and last annotation token of instance
         first_annotation_token = instance['first_annotation_token']
-        first_annotation = nusc.get('sample_annotation', first_annotation_token)
-        movedBefore = first_annotation['movingState']
-
         last_annotation_token = instance['last_annotation_token']
-        next_annotation_token = instance['next']
-        while next_annotation_token != last_annotation_token:
-            current_annotation = nusc.get('sample_annotation', next_annotation_token)
-            movedBefore = current_annotation['']
-            last_annotation = nusc.get('sample_annotation', last_annotation_token)
-            first_annotation = nusc.get('sample_annotation', first_annotation_token)
+        # first annotation of instance
+        first_annotation = loadInitialMovingState.nusc.get('sample_annotation', first_annotation_token)
+        # next annotation token from first_annotation
+        next_annotation_token = first_annotation['next']
 
-        next_annotation_token = instance['next']
-        
-        movedBefore = first_annotation['movedBefore']
+        movedBefore = updateMovedBefore(movedBefore, first_annotation)
+        while next_annotation_token != last_annotation_token and next_annotation_token != '':
+            current_annotation = loadInitialMovingState.nusc.get('sample_annotation', next_annotation_token)
+            movedBefore = updateMovedBefore(movedBefore, current_annotation)
+            current_annotation['movedBefore'] = movedBefore
+            next_annotation_token = current_annotation['next']
+    print('finished loading moved before annotation')
 
-    # if it didin't move iterate over all the sample annotations and see if it moves
+# gets the current moved before state and the annotation
+# looks if the moved before state has changed and inputs movedBefore into the annotation also
+def updateMovedBefore(movedBefore, annotation):
+    if movedBefore == True:
+        movedBefore = True
+    if movedBefore == False and annotation['movingState'] == True:
+        movedBefore = True
+    return movedBefore
 
-    return False
-
-
-
-# this method has to come after the induceMovingStateInSampleAnnotation() method when loading sample_annotations
-# def induceMovedBeforeInSampleAnnotations():
-#     print('start of inducing moved before in sample annotation')
-#     for sample_annotation in nusc.sample_annotation:
-#         movedBefore = getMovedBefore(sample_annotation)
-#         sample_annotation['movedBefore'] = movedBefore
-#     print('end of inducing moved before in sample annotation')
-#     return nusc.sample_annotation
+loadMovedBefore()
