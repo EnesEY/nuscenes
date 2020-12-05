8d5ebcbeeba1c5bca9bb5ecd78f4a97f0655738a
Enes.Yaldiz <enes.yaldiz.bevelop@hd.digital>
2020-12-05 13:52:00 +0100

    quick commit before continuation

--- src/main/load003DistanceToOuterLaneBoundaries.py
+++ src/main/load003DistanceToOuterLaneBoundaries.py
@@ -49,46 +49,84 @@ def extractPointsOfLaneDirection(steps):
     # we create lane arrays with the points mentioned above
     laneToRightLaneBoundary = utils.interpolate(start_point_right_lane[0], start_point_right_lane[1], end_point_right_lane[0], end_point_right_lane[1], steps)
     laneToLeftLaneBoundary = utils.interpolate(start_point_left_lane[0], start_point_left_lane[1], end_point_left_lane[0], end_point_left_lane[1], steps)
-    # 
 
 
+    # get all directions between two points in same direction node array 
+    first_lane = []
+    second_lane = []
+    for i in range(0, len(same_direction_nodes)-1):
+        current_x = same_direction_nodes[i]['x']
+        next_x = same_direction_nodes[i+1]['x']
+        if (current_x - next_x) < 0:
+            first_lane.append(same_direction_nodes[i])
+        if (current_x - next_x) > 0: 
+            second_lane.append(same_direction_nodes[i])
 
-    # load both arrays to poses arrays that will be plotted
-    poses_x = []
-    poses_y = []
-    for value in laneToRightLaneBoundary:
-        poses_x.append(value['x'])
-        poses_y.append(value['y'])
-    for value in laneToLeftLaneBoundary:
-        poses_x.append(value['x'])
-        poses_y.append(value['y'])
+    print(first_lane)
+    print(second_lane)
 
-    # output
-    # plot everything
+
+    poses_first_lane_x = []
+    poses_first_lane_y = []
+    for value in first_lane:
+        poses_first_lane_x.append(value['x'])
+        poses_first_lane_y.append(value['y'])
+
+    
+    poses_second_lane_x = []
+    poses_second_lane_y = []
+    for value in second_lane:
+        poses_second_lane_x.append(value['x'])
+        poses_second_lane_y.append(value['y'])
+
     fig, ax = nusc_map.render_map_patch(my_patch, nusc_map.non_geometric_layers, figsize=(10, 10))
     ax.scatter(x_center, y_center, s=20, c='#33FFFC', alpha=1.0, zorder=2)
-    ax.scatter(poses_x, poses_y, s=20, c='k', alpha=1.0, zorder=2)
+    ax.scatter(poses_first_lane_x, poses_first_lane_y, s=20, c='#33F33C', alpha=1.0, zorder=2)
+    ax.scatter(poses_second_lane_x, poses_second_lane_y, s=20, c='#55533C', alpha=1.0, zorder=2)
+
+
+
+    # # load both arrays to poses arrays that will be plotted
+    # poses_x = []
+    # poses_y = []
+    # for value in same_direction_nodes:
+    #     poses_x.append(value['x'])
+    #     poses_y.append(value['y'])
+    # for value in laneToRightLaneBoundary:
+    #     poses_x.append(value['x'])
+    #     poses_y.append(value['y'])
+    # for value in laneToLeftLaneBoundary:
+    #     poses_x.append(value['x'])
+    #     poses_y.append(value['y'])
+
+    # # output
+    # # plot everything
+    # fig, ax = nusc_map.render_map_patch(my_patch, nusc_map.non_geometric_layers, figsize=(10, 10))
+    # ax.scatter(x_center, y_center, s=20, c='#33FFFC', alpha=1.0, zorder=2)
+    # ax.scatter(poses_x, poses_y, s=20, c='k', alpha=1.0, zorder=2)
 
 
 def getNonInterSectionRoadSegments(road_segments):
+    # setup
     output = []
-
+    # execute
     for road_segment in road_segments:
         if road_segments['is_intersection'] == False:
             output.append(road_segment)
-
+    # output
     return output
 
 def getNodesOfRoadSegments(road_segments, nusc_map):
+    # setup
     output = []
-
+    # execute
     for nodes_token in road_segments['exterior_node_tokens']:
         for node in nusc_map.node:
             if node['token'] == nodes_token:
                 output.append(node)
-    
+    # output
     return output
 
 
 
-extractPointsOfLaneDirection(10)
+extractPointsOfLaneDirection(50)
