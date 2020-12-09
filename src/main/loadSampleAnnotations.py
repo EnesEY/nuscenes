a6c53fd0f0181aa831e1556bfb211e3ff4c7ce17
Enes.Yaldiz <enes.yaldiz.bevelop@hd.digital>
2020-12-09 14:52:22 +0100

    state-before-deleting-files

--- src/main/loadSampleAnnotations.py
+++ src/main/loadSampleAnnotations.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from nuscenes.map_expansion.map_api import NuScenesMap
 from nuscenes.map_expansion import arcline_path_utils
 from multiprocessing import Process
+from tqdm import tqdm
 
 import os
 import time
@@ -26,18 +27,16 @@ class LoadSampleAnnotation:
         self.nusc_map_singapore_queenstown = NuScenesMap(dataroot='/data/sets/nuscenes', map_name='singapore-queenstown')
         self.nusc_map_boston_seaport = NuScenesMap(dataroot='/data/sets/nuscenes', map_name='boston-seaport') 
 
-        print('started filtering process')
-
-        self._load_only_necessary_instances()
-
-        print('finished filtering process')
+        self._filter_unnecessary_instances()
 
         print('started loading annotaions')
         self.processes = []
         self.process_ranges = utils.split_processes(self.nusc.instance)
 
+        thread_number = 0
         for start_and_end in self.process_ranges:
-             self.processes.append(Process(target=self._load_annotations, args=(start_and_end[0], start_and_end[1],)))
+            self.processes.append(Process(target=self._load_annotations, args=(start_and_end[0], start_and_end[1], thread_number,)))
+            thread_number += 1 
 
         for process in self.processes:
             process.start()
@@ -47,15 +46,7 @@ class LoadSampleAnnotation:
         print('finished loading annotaions')
 
 
-
-    def _load_annotations(self, start_index, end_index):
-        self._load_moving_state(start_index, end_index)
-        self._load_moved_before(start_index, end_index)
-        self._load_time_not_moved(start_index, end_index)
-        self._load_in_database(start_index, end_index)
-
-
-    def _load_only_necessary_instances(self):
+    def _filter_unnecessary_instances(self):
         instances = []
         for instance in self.nusc.instance:
             if utils.get_vehicle_and_not_ego_vehicle(instance, self.nusc) == True:
@@ -63,67 +54,136 @@ class LoadSampleAnnotation:
         self.nusc.instance = instances
 
 
-    def _load_moving_state(self, startIndex, endIndex):
-        for index in range(startIndex, endIndex):
+    def _load_annotations(self, start_index, end_index, thread_number):
+        self._load_instance_annotations(start_index, end_index, thread_number)
+        self._load_map_annotations(start_index, end_index, thread_number)
+        self._load_in_database(start_index, end_index)
+
+
+    def _load_instance_annotations(self, start_index, end_index, thread_number):
+        for index in range(start_index, end_index):
             sample_annotations = utils.get_sample_annotations_of_instance(self.nusc.instance[index], self.nusc)
-            for annotation in sample_annotations:
-                velocities = list(self.nusc.box_velocity(annotation['token']))
-                moving = False
-                if abs(np.average(velocities)) > parameters.moving_treshold:
-                    moving = True
-                annotation['movingState'] = moving
+            self._load_moving_state(sample_annotations)
+            self._load_moved_before(sample_annotations)
+            self._load_time_not_moved(sample_annotations)
 
 
-    def _load_moved_before(self, startIndex, endIndex):
-        for index in range(startIndex, endIndex):
-            instance = self.nusc.instance[index]
-            movedBefore = False
-            first_annotation_token = instance['first_annotation_token']
-            last_annotation_token = instance['last_annotation_token']
-            first_annotation = self.nusc.get('sample_annotation', first_annotation_token)
-            next_annotation_token = first_annotation['next']
+    def _load_map_annotations(self, start_index, end_index, thread_number):
+        for index in tqdm(range(start_index, end_index), desc=f'map annotation progress of thread nr. {thread_number}:'):
+            sample_annotations = utils.get_sample_annotations_of_instance(self.nusc.instance[index], self.nusc)
+            nusc_map_name = utils.get_map_name_of_annotation(sample_annotations[0], self.nusc)
+            nusc_map = self._get_map_of_annotation(nusc_map_name)
+            for i in range(0, len(sample_annotations) - 1):
+                road_segment = utils.get_road_segment_of_annotation(sample_annotations[i], nusc_map)
+                self._load_on_intersection(sample_annotations[i], road_segment)
+                self._load_distance_to_boundaries(sample_annotations[i], nusc_map, road_segment, 50)
 
-            if movedBefore == False and first_annotation['movingState'] == True:
+
+    def _load_moving_state(self, sample_annotations):
+        for annotation in sample_annotations:
+            velocities = list(self.nusc.box_velocity(annotation['token']))
+            moving = False
+            if abs(np.average(velocities)) > parameters.moving_treshold:
+                moving = True
+            annotation['movingState'] = moving
+
+
+    def _load_moved_before(self, sample_annotations):
+        movedBefore = False
+        for i in range(0, len(sample_annotations) - 1):
+            if movedBefore == False and sample_annotations[i]['movingState'] == True:
                 movedBefore = True
-            first_annotation['movedBefore'] = movedBefore
-
-            while next_annotation_token != last_annotation_token and next_annotation_token != '':
-                current_annotation = self.nusc.get('sample_annotation', next_annotation_token)
-                if movedBefore == False and current_annotation['movingState'] == True:
-                    movedBefore = True
-                current_annotation['movedBefore'] = movedBefore
-                next_annotation_token = current_annotation['next']
+            sample_annotations[i]['movedBefore'] = movedBefore
 
 
-    def _load_time_not_moved(self, startIndex, endIndex):
-        for index in range(startIndex, endIndex):
-            instance = self.nusc.instance[index]
-            timeNotMoved = 0
-            first_annotation_token = instance['first_annotation_token']
-            last_annotation_token = instance['last_annotation_token']
-            first_annotation = self.nusc.get('sample_annotation', first_annotation_token)
-            next_annotation_token = first_annotation['next']
-
-            if first_annotation['movingState'] == True:
+    def _load_time_not_moved(self, sample_annotations):
+        timeNotMoved = 0
+        for i in range(0, len(sample_annotations) - 1):
+            if sample_annotations[i]['movingState'] == True:
                 timeNotMoved = 0
-            if first_annotation['movingState'] == False:
+            if sample_annotations[i]['movingState'] == False:
                 timeNotMoved += 1
-            first_annotation['timeNotMoved'] = timeNotMoved
+            sample_annotations[i]['timeNotMoved'] = timeNotMoved
 
-            while next_annotation_token != last_annotation_token and next_annotation_token != '':
-                current_annotation = self.nusc.get('sample_annotation', next_annotation_token)
-                if current_annotation['movingState'] == True:
-                    timeNotMoved = 0
-                if current_annotation['movingState'] == False:
-                    timeNotMoved += 1
-                current_annotation['timeNotMoved'] = timeNotMoved
-                next_annotation_token = current_annotation['next']
+
+    def _load_on_intersection(self, annotation, road_segment):
+        onIntersection = ''
+        if road_segment != '':
+            onIntersection = road_segment['is_intersection']
+        annotation['onIntersection'] = onIntersection
+
+
+    def _load_distance_to_boundaries(self, annotation, nusc_map, road_segment, sampling_rate):
+        distance_to_left_boundary = ''
+        distance_to_right_boundary = ''
+
+        nusc_map.non_geometric_polygon_layers = ['drivable_area', 'road_segment', 'lane']
+        nusc_map.non_geometric_line_layers = []
+        nusc_map.non_geometric_layers = ['drivable_area', 'road_segment', 'lane']
+
+        skip = False
+        if road_segment == '' or road_segment['is_intersection'] == True:
+            skip = True
+        
+        nearestLane = utils.get_closest_lane_of_annotaion(annotation, nusc_map)
+        if nearestLane == '':
+            skip = True
+
+        if skip != True:
+            x_center = annotation['translation'][0]
+            y_center = annotation['translation'][1]
+
+            # get direction of x and y of the nearest lane
+            [delta_x_lane, delta_y_lane] = utils.get_delta_x_and_delta_y_of_lane(annotation, nearestLane)
+            [orthogonal_delta_x_lane , orthogonal_delta_y_lane] = [delta_y_lane, (-1)*delta_x_lane]
+            road_segment_nodes = utils.get_nodes_of_road_segment(road_segment, nusc_map)
+            [same_direction_nodes, opposite_direction_nodes] = utils.get_opposite_and_same_direction_of_node_array(road_segment_nodes, delta_x_lane, delta_y_lane)
+                    
+            # Get average point of both lanes
+            [average_x_same_direction, average_y_same_direction] = utils.get_average_point_of_node_array(same_direction_nodes)
+            [average_x_opposite_direction, average_y_opposite_direction] = utils.get_average_point_of_node_array(opposite_direction_nodes)
+
+            isLeftBoundaryValid = True
+            if average_x_opposite_direction == 0 or average_y_opposite_direction == 0:
+                isLeftBoundaryValid = False
+                    
+            isRightBoundaryValid = True
+            if average_x_same_direction == 0 or average_y_same_direction == 0:
+                isRightBoundaryValid = False
+
+            if isLeftBoundaryValid == True:
+                start_point_left_lane = [x_center, y_center]
+                end_point_left_lane =  [x_center + ((-1) * orthogonal_delta_x_lane) , y_center + ((-1) * orthogonal_delta_y_lane)]
+                lane_to_left_boundary = utils.interpolate(start_point_left_lane[0], start_point_left_lane[1], end_point_left_lane[0], end_point_left_lane[1], sampling_rate)
+                closest_point_of_left_lane_to_opposite_average_point = utils.get_nearest_node_of_node_array_to_point(lane_to_left_boundary, average_x_opposite_direction, average_y_opposite_direction)
+                distance_to_left_boundary = utils.get_distance_between_two_points(closest_point_of_left_lane_to_opposite_average_point['x'],closest_point_of_left_lane_to_opposite_average_point['y'],x_center, y_center)
+
+            if isRightBoundaryValid == True:
+                start_point_right_lane = [x_center, y_center] 
+                end_point_right_lane = [x_center + orthogonal_delta_x_lane , y_center + orthogonal_delta_y_lane]
+                lane_to_right_boundary = utils.interpolate(start_point_right_lane[0], start_point_right_lane[1], end_point_right_lane[0], end_point_right_lane[1], sampling_rate)
+                closest_point_of_right_lane_to_same_average_point = utils.get_nearest_node_of_node_array_to_point(lane_to_right_boundary, average_x_same_direction, average_y_same_direction)
+                distance_to_right_boundary = utils.get_distance_between_two_points(closest_point_of_right_lane_to_same_average_point['x'],closest_point_of_right_lane_to_same_average_point['y'],x_center, y_center)
+
+        annotation['distanceToLeftBoundary'] = distance_to_left_boundary
+        annotation['distanceToRightBoundary'] = distance_to_right_boundary
 
 
     def _load_in_database(self, startIndex, endIndex):
         for index in range(startIndex, endIndex):
             sample_annotations = utils.get_sample_annotations_of_instance(self.nusc.instance[index], self.nusc)
             self.db.sample_annotations.insert_many(sample_annotations)
+
+
+    def _get_map_of_annotation(self, map_name):
+        if map_name == 'singapore-onenorth':
+            return self.nusc_map_singapore_onenorth
+        if map_name == 'singapore-hollandvillage':
+            return self.nusc_map_singapore_hollandvillage
+        if map_name == 'singapore-queenstown':
+            return self.nusc_map_singapore_queenstown
+        if map_name == 'boston-seaport':
+            return self.nusc_map_boston_seaport
 
 
 myInstance = LoadSampleAnnotation()
--- src/main/sample_annotation_utils.py
+++ src/main/sample_annotation_utils.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 
 
+
 def split_processes(arrayToSplit):
     start_and_end_of_processes = []
     step_length = math.floor(len(arrayToSplit)/os.cpu_count())
@@ -13,12 +14,15 @@ def split_processes(arrayToSplit):
     rest = len(arrayToSplit)%os.cpu_count()
     for i in range(0, os.cpu_count()):
         if i > rest:
-            i = rest
-        start_and_end_of_processes[i][0] += i
-        start_and_end_of_processes[i][1] += i+1
+            start_and_end_of_processes[i][0] += rest
+            start_and_end_of_processes[i][1] += rest+1
+        else:
+            start_and_end_of_processes[i][0] += i
+            start_and_end_of_processes[i][1] += i+1
+
     start_and_end_of_processes[os.cpu_count()-1][1] -= 1
-    
     return start_and_end_of_processes
+
 
 def get_sample_annotations_of_instance(instance, nusc):
     output = []
@@ -34,6 +38,7 @@ def get_sample_annotations_of_instance(instance, nusc):
         next_annotation_token = current_annotation['next']
     return output
 
+
 def get_vehicle_and_not_ego_vehicle(instance, nusc):
     first_annotation_token = instance['first_annotation_token']
     annotation = nusc.get('sample_annotation', first_annotation_token)
@@ -45,3 +50,118 @@ def get_vehicle_and_not_ego_vehicle(instance, nusc):
             return True
     else:
         return False
+
+
+def get_map_name_of_annotation(annotation, nusc):
+    sample_token = annotation['sample_token']
+    sample = nusc.get('sample', sample_token)
+    scene_token = sample['scene_token']
+    scene = nusc.get('scene', scene_token)
+    log_token = scene['log_token']
+    log = nusc.get('log', log_token)
+    map_name = log['location']
+    return map_name
+
+
+def get_road_segment_of_annotation(annotation, nusc_map):
+    output = ''
+    layer_type = 'road_segment'
+    layers = nusc_map.layers_on_point(annotation['translation'][0], annotation['translation'][1])
+    if layers[layer_type] == '':
+        return output
+
+    table = nusc_map.road_segment
+    if table == '':
+        return output
+
+    for element in table:
+        if element['token'] == layers[layer_type]:
+            output = element
+
+    return output
+
+
+def get_closest_lane_of_annotaion(annotation, nusc_map):
+    output = ''
+
+    closest_lane_token = nusc_map.get_closest_lane(annotation['translation'][0], annotation['translation'][1], radius=2)
+    if closest_lane_token == '':
+        return output
+
+    output = nusc_map.get_lane(closest_lane_token)
+
+    return output
+
+
+def get_delta_x_and_delta_y_of_lane(annotation, lane):
+    start_x = lane[0]['start_pose'][0]
+    start_y = lane[0]['start_pose'][1]
+    end_x = lane[0]['end_pose'][0]
+    end_y = lane[0]['end_pose'][1]
+
+    delta_x = (end_x - start_x)
+    delta_y = (end_y - start_y)
+
+    return [delta_x, delta_y]
+
+def get_nodes_of_road_segment(road_segment, nusc_map):
+    output = []
+    for nodes_token in road_segment['exterior_node_tokens']:
+        for node in nusc_map.node:
+            if node['token'] == nodes_token:
+                output.append(node)
+    return output
+
+
+def get_opposite_and_same_direction_of_node_array(node_array, delta_x_lane, delta_y_lane):
+    same_direction_nodes = []
+    opposite_direction_nodes = []
+    for i in range(0, len(node_array)-1):
+        current_delta_x = node_array[i+1]['x'] - node_array[i]['x']
+        current_delta_y = node_array[i+1]['y'] - node_array[i]['y']
+        if (delta_x_lane * current_delta_x) > 0 and (delta_y_lane * current_delta_y) > 0:
+            same_direction_nodes.append(node_array[i])
+        if (delta_x_lane * current_delta_x) < 0 and (delta_y_lane * current_delta_y) < 0:
+            opposite_direction_nodes.append(node_array[i])
+    return [same_direction_nodes, opposite_direction_nodes]
+
+
+def get_average_point_of_node_array(node_array):
+    average_x = 0
+    average_y = 0
+    for point in node_array:
+        average_x += point['x']
+        average_y += point['y']
+    if len(node_array) != 0:
+        average_x = (average_x/len(node_array))
+        average_y = (average_y/len(node_array))
+    return [average_x, average_y]
+
+
+def interpolate(x_start, y_start, x_end, y_end, steps):
+    output = []
+    deltaX = (x_end - x_start)
+    deltaY = (y_end - y_start)
+
+    for i in range(0, steps):
+        output.append({ 'x': (x_start + ((deltaX/steps)*i)), 'y': (y_start + ((deltaY/steps)*i))})
+
+    return output
+
+def get_nearest_node_of_node_array_to_point(node_array, x_of_point, y_of_point):
+    max = 999999
+    for point in node_array:
+        delta_x = x_of_point - point['x']
+        delta_y = y_of_point - point['y']
+        distance = math.sqrt(pow(delta_x, 2) + pow(delta_y, 2))
+        if distance < max:
+            max = distance
+            closest_point = point
+    return closest_point
+
+def get_distance_between_two_points(x1, y1, x2, y2):
+    delta_x = x2 - x1
+    delta_y = y2 - y1
+
+    distance = math.sqrt(pow(delta_x, 2) + pow(delta_y, 2))
+    return distance
