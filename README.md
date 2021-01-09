c53b7bbc94b9afc94755a0c3a42be8998586035d
Enes Yaldiz <enes.yaldiz94@gmail.com>
2021-01-09 22:50:57 +0100

    commit-before-collab

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# nuscenes
+# Project to the Bachelor Thesis of Enes Yalldiz
+
+## usable to create a neural network that classifies motionless vehicles as either parked or stopped
--- src/main/loadDatabase.py
+++ src/main/loadDatabase.py
@@ -10,52 +10,52 @@ import numpy as np
 import load_annotations
 import utils as utils
 
-nusc = NuScenes(version='v1.0-mini', dataroot='/data/sets/nuscenes', verbose=True)
+nusc = NuScenes(version='v1.0-trainval', dataroot='/data/sets/nuscenes', verbose=True)
 dbPath = 'mongodb+srv://enesey:485f6483e3c8666b72fda603a7f87006b83549a54395f2504eb58935f35d00d9@nuscenescluster.jh1vw.mongodb.net/test'
 # dbPath = 'mongodb://localhost:27017/nuscenes'
 client=MongoClient(dbPath)
 db = client.nuscenes
 
 def loadDatabase():
-    #load scene
-    db.scene.insert_many(nusc.scene)
-    print('loaded scene complete')
-    #load sample
-    db.sample.insert_many(nusc.sample)
-    print('loaded sample complete')
-    #load sample_data
-    db.sample_data.insert_many(nusc.sample_data)
-    print('loaded sample_data complete')
-    #load ego_pose
-    db.ego_pose.insert_many(nusc.ego_pose)
-    print('loaded ego_pose complete')
-    #load sample_annotation (unmodified)
-    db.sample_annotation.insert_many(nusc.sample_annotation)
-    print('loaded unmodified sample annotations')
-    #load sample_annotation (unmodified)
+    # #load scene
+    # db.scene.insert_many(nusc.scene)
+    # print('loaded scene complete')
+    # #load sample
+    # db.sample.insert_many(nusc.sample)
+    # print('loaded sample complete')
+    # #load sample_data
+    # db.sample_data.insert_many(nusc.sample_data)
+    # print('loaded sample_data complete')
+    # #load ego_pose
+    # db.ego_pose.insert_many(nusc.ego_pose)
+    # print('loaded ego_pose complete')
+    # #load sample_annotation (unmodified)
+    # db.sample_annotation.insert_many(nusc.sample_annotation)
+    # print('loaded unmodified sample annotations')
+    # #load sample_annotation (unmodified)
     load_annotations.load_features(dbPath)
     print('loaded modified sample annotations')
-    #load attribute
-    db.attribute.insert_many(nusc.attribute)
-    print('loaded attribute complete')
-    #load visibility
-    db.visibility.insert_many(nusc.visibility)
-    print('loaded visibility complete')
-    #load instance
-    db.instance.insert_many(nusc.instance)
-    print('loaded instance complete')
-    #load category
-    db.category.insert_many(nusc.category)
-    print('loaded category complete')
-    #load log
-    db.log.insert_many(nusc.log)
-    print('loaded log complete')
-    #load calibrated_sensor
-    db.calibrated_sensor.insert_many(nusc.calibrated_sensor)
-    print('loaded calibrated_sensor complete')
-    #load sensor
-    db.sensor.insert_many(nusc.sensor)
-    print('loaded sensor complete')
+    # #load attribute
+    # db.attribute.insert_many(nusc.attribute)
+    # print('loaded attribute complete')
+    # #load visibility
+    # db.visibility.insert_many(nusc.visibility)
+    # print('loaded visibility complete')
+    # #load instance
+    # db.instance.insert_many(nusc.instance)
+    # print('loaded instance complete')
+    # #load category
+    # db.category.insert_many(nusc.category)
+    # print('loaded category complete')
+    # #load log
+    # db.log.insert_many(nusc.log)
+    # print('loaded log complete')
+    # #load calibrated_sensor
+    # db.calibrated_sensor.insert_many(nusc.calibrated_sensor)
+    # print('loaded calibrated_sensor complete')
+    # #load sensor
+    # db.sensor.insert_many(nusc.sensor)
+    # print('loaded sensor complete')
     load_preprocessed_features()
 
 
